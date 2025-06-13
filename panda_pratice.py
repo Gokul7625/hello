063b60b68adb0945c3affe24c7b0495803dc4fd7
Gokul7625 <kannansaravanan09@gmail.com>
2025-06-13 20:26:54 +0530

    new update 1

--- panda_pratice.py
+++ panda_pratice.py
@@ -2,4 +2,4 @@ import pandas as pd
 data = {"name": ["gk", "mara","d","f","gg","dg"], "age": [20, 20,3,6,7,8]}
 df = pd.DataFrame(data)
 print(df.info())
-print(df.desccribe())
+print(df.describe())
